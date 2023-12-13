337d24d360946e2b8a24c48d2ed0df23d0938a23
MusiliC <musilibrian07@gmail.com>
2023-12-14 00:23:44 +0300

    Added form validation for all forms in admin side

--- src/modules/admin/components/CreateFixtureForm.jsx
+++ src/modules/admin/components/CreateFixtureForm.jsx
@@ -2,21 +2,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import OutlineButton from "../../../components/common/OutlineButton";
+import { useForm } from "react-hook-form";
+import FormInputError from "../../../components/common/FormInputError";
 
 const CreateFixtureForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="formTicketContainer">
+    <form className="formTicketContainer" onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="fixtureType" className="labelStyling">
           Select fixture type
         </label>
-        <select name="fixtureType" id="fixtureType" className="inputStyling">
+        <select
+          name="fixtureType"
+          {...register("fixtureType", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
+          id="fixtureType"
+          className="inputStyling"
+        >
           <option value="" selected disabled hidden>
             Fixture type
           </option>
           <option value="Afcon">Afcon</option>
           <option value="KPL">KPL</option>
         </select>
+
+        {errors?.fixtureType && (
+          <FormInputError message={errors?.fixtureType?.message} />
+        )}
       </div>
       {/* fixture time */}
       <div>
@@ -26,10 +52,18 @@ const CreateFixtureForm = () => {
         <input
           type="time"
           name="fixtureTime"
+          {...register("fixtureTime", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
           id="fixtureTime"
           className="inputStyling"
-          required=""
         />
+        {errors?.fixtureTime && (
+          <FormInputError message={errors?.fixtureTime?.message} />
+        )}
       </div>
 
       {/* location */}
@@ -41,9 +75,17 @@ const CreateFixtureForm = () => {
           type="text"
           name="fixtureLocation"
           id="fixtureLocation"
+          {...register("fixtureLocation", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
           className="inputStyling"
-          required=""
         />
+        {errors?.fixtureLocation && (
+          <FormInputError message={errors?.fixtureLocation?.message} />
+        )}
       </div>
       {/* homeTeam */}
       <div>
@@ -54,9 +96,17 @@ const CreateFixtureForm = () => {
           type="text"
           name="homeTeam"
           id="homeTeam"
+          {...register("homeTeam", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
           className="inputStyling"
-          required=""
         />
+        {errors?.homeTeam && (
+          <FormInputError message={errors?.homeTeam?.message} />
+        )}
       </div>
       {/* awayTeam */}
       <div>
@@ -67,25 +117,42 @@ const CreateFixtureForm = () => {
           type="text"
           name="awayTeam"
           id="awayTeam"
+          {...register("awayTeam", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
           className="inputStyling"
-          required=""
         />
+        {errors?.awayTeam && (
+          <FormInputError message={errors?.awayTeam?.message} />
+        )}
       </div>
 
       {/* fixture date */}
       <div>
         <label htmlFor="fixtureDate" className="labelStyling">
-          Home Team
+          Fixture Date
         </label>
         <input
           type="date"
           name="fixtureDate"
+          {...register("fixtureDate", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
           id="fixtureDate"
           className="inputStyling"
-          required=""
         />
+        {errors?.fixtureDate && (
+          <FormInputError message={errors?.fixtureDate?.message} />
+        )}
       </div>
       <div className="flex">
+        <button>create</button>
         <Link>
           <OutlineButton text={"Create Fixture"} />
         </Link>
--- src/modules/admin/components/TicketManageForm.jsx
+++ src/modules/admin/components/TicketManageForm.jsx
@@ -1,97 +1,192 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import OutlineButton from '../../../components/common/OutlineButton'
-import { Link } from 'react-router-dom'
+import React from "react";
+import OutlineButton from "../../../components/common/OutlineButton";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import FormInputError from "../../../components/common/FormInputError";
 
 const TicketManageForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <form className="formTicketContainer">
-    <div>
-      <label htmlFor="fixtureType" className="labelStyling">
-        Select fixture type
-      </label>
-      <select name="fixtureType" id="fixtureType" className="inputStyling">
-        <option value="" selected disabled hidden>
-          Fixture type
-        </option>
-        <option value="Afcon">Afcon</option>
-        <option value="KPL">KPL</option>
-      </select>
-    </div>
-    {/* fixture time */}
-    <div>
-      <label htmlFor="totalTickets" className="labelStyling">
-        Total Tickets Allocated
-      </label>
-      <input
-        type="number"
-        name="totalTickets"
-        id="totalTickets"
-        className="inputStyling"
-        required=""
-      />
-    </div>
+    <form className="formTicketContainer" onSubmit={handleSubmit(onSubmit)} >
+      <div>
+        <label htmlFor="fixtureType" className="labelStyling">
+          Select fixture type
+        </label>
+        <select
+          name="fixtureType"
+          id="fixtureType"
+          {...register("fixtureType", {
+            required: {
+              value: true,
+              message: "Field is required",
+            },
+          })}
+          className="inputStyling"
+        >
+          <option value="" selected disabled hidden>
+            Fixture type
+          </option>
+          <option value="Afcon">Afcon</option>
+          <option value="KPL">KPL</option>
+        </select>
+        {errors?.fixtureType && (
+          <FormInputError message={errors?.fixtureType?.message} />
+        )}
+      </div>
+      {/* fixture time */}
+      <div>
+        <label htmlFor="totalTickets" className="labelStyling">
+          Total Tickets Allocated
+        </label>
+        <input
+          type="number"
+          name="totalTickets"
+          id="totalTickets"
+          {...register("totalTickets", {
+            required: "Field is required",
+            min: {
+              value: 1,
+              message: "Number of tickets should be at-least 1",
+            },
+          })}
+          className="inputStyling"
+        />
+        {errors?.totalTickets && (
+          <div>
+            <FormInputError message={errors?.totalTickets?.message} />
+            {errors?.totalTickets?.type === "minLength" && (
+              <FormInputError message={errors?.totalTickets?.message} />
+            )}
+          </div>
+        )}
+      </div>
 
-    {/* location */}
-    <div>
-      <label htmlFor="totalVip" className="labelStyling">
-       Total VIP Tickets
-      </label>
-      <input
-        type="number"
-        name="totalVip"
-        id="totalVip"
-        className="inputStyling"
-        required=""
-      />
-    </div>
-    {/* homeTeam */}
-    <div>
-      <label htmlFor="vipAmount" className="labelStyling">
-        Vip Ticket Amount
-      </label>
-      <input
-        type="number"
-        name="vipAmount"
-        id="vipAmount"
-        className="inputStyling"
-        required=""
-      />
-    </div>
-    {/* awayTeam */}
-    <div>
-      <label htmlFor="totalNormal" className="labelStyling">
-        Total Normal Tickets
-      </label>
-      <input
-        type="number"
-        name="totalNormal"
-        id="totalNormal"
-        className="inputStyling"
-        required=""
-      />
-    </div>
+      {/* location */}
+      <div>
+        <label htmlFor="totalVip" className="labelStyling">
+          Total VIP Tickets
+        </label>
+        <input
+          type="number"
+          name="totalVip"
+          id="totalVip"
+          {...register("totalVip", {
+            required: "Field is required",
+            min: {
+              value: 1,
+              message: "Number of VIP tickets should be at-least 1",
+            },
+          })}
+          className="inputStyling"
+        />
+        {errors?.totalVip && (
+          <div>
+            <FormInputError message={errors?.totalVip?.message} />
+            {errors?.totalVip?.type === "minLength" && (
+              <FormInputError message={errors?.totalVip?.message} />
+            )}
+          </div>
+        )}
+      </div>
+      {/* homeTeam */}
+      <div>
+        <label htmlFor="vipAmount" className="labelStyling">
+          Vip Ticket Amount
+        </label>
+        <input
+          type="number"
+          name="vipAmount"
+          {...register("vipAmount", {
+            required: "Field is required",
+            min: {
+              value: 1,
+              message: "VIP amount should be at-least 1=/",
+            },
+          })}
+          id="vipAmount"
+          className="inputStyling"
+        />
+        {errors?.vipAmount && (
+          <div>
+            <FormInputError message={errors?.vipAmount?.message} />
+            {errors?.vipAmount?.type === "minLength" && (
+              <FormInputError message={errors?.vipAmount?.message} />
+            )}
+          </div>
+        )}
+      </div>
+      {/* Normal tickets */}
+      <div>
+        <label htmlFor="totalNormal" className="labelStyling">
+          Total Normal Tickets
+        </label>
+        <input
+          type="number"
+          name="totalNormal"
+          id="totalNormal"
+          {...register("totalNormal", {
+            required: "Field is required",
+            min: {
+              value: 1,
+              message: "Number of Normal tickets should be at-least 1",
+            },
+          })}
+          className="inputStyling"
+        />
+        {errors?.totalNormal && (
+          <div>
+            <FormInputError message={errors?.totalNormal?.message} />
+            {errors?.totalNormal?.type === "minLength" && (
+              <FormInputError message={errors?.totalNormal?.message} />
+            )}
+          </div>
+        )}
+      </div>
 
-    {/* fixture date */}
-    <div>
-      <label htmlFor="normalAmount" className="labelStyling">
-        Normal Ticket Amount
-      </label>
-      <input
-        type="number"
-        name="normalAmount"
-        id="normalAmount"
-        className="inputStyling"
-        required=""
-      />
-    </div>
-    <div className="flex">
-      <Link>
-        <OutlineButton text={"Create Fixture"} />
-      </Link>
-    </div>
-  </form>
-  )
-}
+      {/* fixture date */}
+      <div>
+        <label htmlFor="normalAmount" className="labelStyling">
+          Normal Ticket Amount
+        </label>
+        <input
+          type="number"
+          name="normalAmount"
+          id="normalAmount"
+          {...register("normalAmount", {
+            required: "Field is required",
+            min: {
+              value: 1,
+              message: "Normal amount should be at-least 1=/",
+            },
+          })}
+          className="inputStyling"
+        />
+        {errors?.normalAmount && (
+          <div>
+            <FormInputError message={errors?.normalAmount?.message} />
+            {errors?.normalAmount?.type === "minLength" && (
+              <FormInputError message={errors?.normalAmount?.message} />
+            )}
+          </div>
+        )}
+      </div>
+      <div className="flex">
+        <button>create event</button>
+        <Link>
+          <OutlineButton text={"Create event"} />
+        </Link>
+      </div>
+    </form>
+  );
+};
 
-export default TicketManageForm
+export default TicketManageForm;
